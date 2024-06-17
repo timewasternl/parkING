843135ab1693bf8ab5742725b02ea3f444f6537b
Sven Boekelder <sven.boekelder@ing.com>
2024-06-17 08:33:34 +0200

    Removed useless code, added automatically opening parking selection

--- parkeerscriptje.js
+++ parkeerscriptje.js
@@ -1,57 +1,25 @@
 const FORM_PREFIX = "buildingBlocks:inlineBuildingBlocks:block1:form:";
 
-// const suggestedEmployee = localStorage.getItem("employee") || '<ING username>, <lastname>, <firstname>';
-// const employee = prompt("Naam:", suggestedEmployee);
-// localStorage.setItem("employee", employee);
-
 const suggestedDate = localStorage.getItem("date") || new Date(Date.now() + ( 3600 * 1000 * 24)).toLocaleDateString('nl-NL');
 const date = prompt("Datum:", suggestedDate);
 localStorage.setItem("date", date);
 
-const suggestedParking = localStorage.getItem("parking");
-const parking = prompt("Parkeerplaats:", suggestedParking);
-localStorage.setItem("parking", parking);
-
-// await setupEmployee();
 await setupParking();
 await setFieldValues();
 
-async function setupEmployee(){
-    document.getElementsByName(`${FORM_PREFIX}fieldRepeater:1:field:fieldContent:field-editor:textfield`)[0].dispatchEvent(new Event('click'));
-    document.getElementsByName(`${FORM_PREFIX}fieldRepeater:1:field:fieldContent:field-editor:textfield`)[0].setAttribute('value', employee);
-
-
-    await new Promise(r => setTimeout(r, 1000));
-
-    Array.from(employee).forEach(char => {
-
-        console.log(char)
-
-        const event = new KeyboardEvent('keyup', {
-            key: char,
-          });
-        document.getElementsByName(`${FORM_PREFIX}fieldRepeater:1:field:fieldContent:field-editor:textfield`)[0].dispatchEvent(event);
-
-    })
-    await new Promise(r => setTimeout(r, 10000));
-}
-
 async function setupParking(){
     document.getElementsByName(`${FORM_PREFIX}fieldGroupRepeater:1:fieldGroup:fieldRepeater:1:field:fieldContent:field-editor:parkingArea:parkingarea_checkbox`)[0].click();
-    document.getElementsByName(`${FORM_PREFIX}fieldRepeater:1:field:fieldContent:field-editor:textfield`)[0].click();
-
-    // Timeout to activate input fields
-    await new Promise(r => setTimeout(r, 500));
+    await new Promise(r => setTimeout(r, 1000));
+    document.querySelector(`.pss_fieldname_parkingarearef .pss_selectvalue_link`).click();
 }
 
-
 async function setFieldValues(){
+    document.querySelector(`.pss_fieldname_internalvisitorref .pss_selectvalue_link`).click();
+
     const changeValues = [
-        // {name: "employee", element: "fieldRepeater:1:field:fieldContent:field-editor:textfield", value: employee},
         {name: "date", element: "fieldGroupRepeater:0:fieldGroup:fieldRepeater:1:field:fieldContent:field-editor:datetimepicker", value: date},
         {name: "startTime", element: "fieldGroupRepeater:0:fieldGroup:fieldRepeater:2:field:fieldContent:field-editor:datetimepicker", value: "08:00"},
         {name: "endTime", element: "fieldGroupRepeater:0:fieldGroup:fieldRepeater:3:field:fieldContent:field-editor:datetimepicker", value: "09:00"},
-        {name: "parking", element: "fieldGroupRepeater:1:fieldGroup:fieldRepeater:1:field:fieldContent:field-editor:parkingArea:textfield", value: parking},
     ]
     
     changeValues.forEach(change => {
